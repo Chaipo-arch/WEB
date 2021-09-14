3f4cc999181d68eb7a7bd95559f7ce6938ae62f6
Chaipo-arch <81694579+Chaipo-arch@users.noreply.github.com>
2021-09-14 14:00:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function getValue(){
+    var nom = document.getElementById("nom").value;
+    var prenom = document.getElementById("prenom").value
+    if (nom != "Bribach " || nom != "bribach"){
+        alert("tu n'a rien a faire sur ce site")
+    }
+    if (prenom != "Ahmed" || prenom != "ahmed"){
+        alert("tu n'a rien a faire sur ce site")
+    }
+}
