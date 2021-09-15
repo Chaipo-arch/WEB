215d9f746049b98daa756fb65ab51783ae63faaf
Chaipo-arch <81694579+Chaipo-arch@users.noreply.github.com>
2021-09-15 18:34:47 +0200

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,6 +1,19 @@
-let dollars = 0;
+let dollars = 0;        // la monnaie du joueur
+document.getElementById("dollars")
+/**
+ * fonction permettant d'incrementer de 1$ toute les 10 seconde
+ */
 function augmentation(){
     dollars ++;
+    Element.toString(dollars)
+
 }
-window.setInterval(augmentation(),1);
-document.write(dollars +"$")
+
+/**
+ * fonction qui fait qu'a chaque fois que l'utilisateur appuie sur le bouton mineur il gagne 1$
+ */
+function onMine(){
+    dollars++;
+    document.location.reload();
+    Element.toString(dollars)
+}
